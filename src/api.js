25ecaa2f380e91a362a2c07e0b3636002d71c7f1
Vladimir <DistoRRRtion@gmail.com>
2022-12-09 19:14:06 +0200

    3. Settion up our API

---
+++ src/api.js
@@ -0,0 +1,37 @@
+// API KEY = cf9a8ce674fa497bb97efa1ab44c260f
+const YOUR_API_KEY = '?key=cf9a8ce674fa497bb97efa1ab44c260f';
+
+// getting the date
+const getCurrentMonth = () => {
+    const month = new Date().getMonth() + 1;
+    if (month < 10) {
+        return `0${month}`;
+    } else {
+        return month;
+    }
+}
+const getCurrentDay = () => {
+    const day = new Date().getDate();
+    if (day < 10) {
+        return `0${day}`;
+    } else {
+        return day;
+    }
+}
+
+// Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear-1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear+1}-${currentMonth}-${currentDay}`;
+
+// base url 
+const base_url = 'https://api.rawg.io/api/';
+
+// Popular Games
+const popular_games = `games${YOUR_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+
+const popularGamesURL = () => `${base_url}${popular_games}`;
+console.log(popularGamesURL());
